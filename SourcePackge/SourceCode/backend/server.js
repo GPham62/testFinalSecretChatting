df6ce1e84b02e20f41cd837897d79d579df34912
Lan Dang <danghoanglan271@gmail.com>
2019-02-23 02:20:44 +0700

    Connect to mLab || mongodb server localhost

--- SourcePackge/SourceCode/backend/server.js
+++ SourcePackge/SourceCode/backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/api-secret-chating'
+mongoose.connect(URL, {useNewUrlParser: true}, () => {
+    console.log(`Connect to mongodb server at ${URL}`)
+})
 
 const apiRoutes = require('./api_routes')
 
