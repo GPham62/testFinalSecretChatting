6013e08c6e9470a24469e6041d76279735da6b2e
Lan Dang <danghoanglan271@gmail.com>
2019-02-23 01:21:30 +0700

    Modify server.js file

---
+++ SourcePackge/SourceCode/backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.port || 3000
+
+const apiRoutes = require('./api_routes')
+
+app.use('/api', apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`)
+})
