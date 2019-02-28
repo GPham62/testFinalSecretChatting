9a7c15754ef866db801ee9814d66e10e93fa0487
Lan Dang <danghoanglan271@gmail.com>
2019-02-28 23:10:30 +0700

    Add redux to app

--- SourcePackge/SourceCode/client/src/App.js
+++ SourcePackge/SourceCode/client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {connect} from 'react-redux'
+import openSocket from 'socket.io-client'
+
+import {connectedToSocket} from './redux/actionCreator'
 
 //import LoginScreen from './containers/LoginScreen';
 import Navbar from './components/navbar/Navbar';
 import SignIn from './components/signin/SignIn';
 
+const socketURL = 'http://localhost:5000'
+const socket = openSocket(socketURL)
+
+
 class App extends Component {
   onLogin = () => {
-    window.location.href="http://localhost:3000/api/auth/fb";
+    window.location.href="http://localhost:5000/api/auth/fb";
+  }
+  componentWillMount() {
+    this.props.dispatch(connectedToSocket(socket))
   }
   render() {
     return (
@@ -21,4 +32,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  chat: state.chatReducer
+})
+
+
+export default connect(mapStateToProps)(App) ;
